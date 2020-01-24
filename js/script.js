53a65be35dc435a839aebfc300010b8fd8c4af58
StevenVallarsa <orionsjv@gmail.com>
2020-01-23 22:47:19 -0500

    Started Trying DOM Manipulation

--- js/script.js
+++ js/script.js
@@ -2,23 +2,15 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
+
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+/* * *
+ * global variables
+ * * */
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-
-
+const studentList = document.querySelectorAll(".student-item");
+let studentsPerPage = 10;
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
@@ -35,16 +27,24 @@ FSJS project 2 - List Filter and Pagination
        "invoke" the function 
 ***/
 
+function showPage(studentList, page) {
+  let startIndex = page * studentsPerPage - studentsPerPage;
+  let endIndex = page * studentsPerPage;
 
+  for (let i = 0; i < studentList.length; i++) {
+    if (i < startIndex && i > endIndex) {
+      studentList[i].style.display = "none";
+    } else {
+      studentList[i].style.display = "";
+    }
+  }
+}
 
+showPage(studentList, 1);
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
-
-
-
-
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
